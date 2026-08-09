43520f4c17a9ebf8eb75ede721a9e1ebc5dc3682
simplihamza <simplihamza@gmail.com>
2026-08-10 02:24:01 +0300

    Fixed my dictionary comprehension

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pandas
+
 nato_alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
-data_frame = pandas.DataFrame(nato_alphabets)
+phonetic_helper = {row.letter:row.code for (index, row) in nato_alphabets.iterrows()}
 
-phonetic_helper = {index:row for (index, row) in data_frame.iterrows()}
-print(phonetic_helper)
+user_input = input("Enter the word or name: ").upper()
+#
+# for letter in user_input:
+#     if letter in phonetic_helper["letter"]:
+#         print(phonetic_helper[letter])
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+# TODO: Use a LIST comprehension to build a list of NATO code words —
+#  one for each letter in the user's input word, by looking each letter
+#  up in your dictionary.
